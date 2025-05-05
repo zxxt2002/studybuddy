2a589be3da044753c97023160c1ba61884c0de80
jacobwliang <jacob.w.liang@gmail.com>
2025-05-05 00:17:01 -0500

    added bootstrap5

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 const root = createRoot(document.getElementById('root'))
